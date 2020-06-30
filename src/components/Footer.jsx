8c352bfb76481ceb109afdf50c9f3faec454e2fb
Matt <mw15macf@alum.siena.edu>
2020-06-30 00:22:24 -0400

    Created Keeper App UI

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const year = new Date().getFullYear()
+
+function Footer() {
+    return <footer>
+            <p>Copyright {year} © </p>
+           </footer>;
+}
+
+export default Footer;
